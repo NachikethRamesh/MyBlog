f933bc891de66efd6285b4fcb6da8246150dcc05
NachikethRamesh <nrames609@gmail.com>
2019-10-15 17:36:29 +0530

    new updates

---
+++ Client/client.js
@@ -0,0 +1,59 @@
+const backendServerShowAllTitles = 'http://localhost:5002/main';
+
+const allPostsTag = document.getElementById('allPosts');
+
+document.onload((event) => {
+    event.preventDefault();
+
+    const postObject = {
+        'ping': 'pageReq'
+    };
+
+    reqData(postObject);
+});
+
+async function reqData(dataObject) {
+    //get article details from server
+    let response = await fetch(backendServerShowAllTitles, {
+        method: 'POST',
+        body: JSON.stringify(dataObject),
+        headers: {
+            'content-type': 'application/JSON'
+        }
+    });
+
+    let json = await response.json();
+
+    var pingback = JSON.parse(json);
+
+    //Remove all post tags and refresh everytime page is reloaded
+    this.removeTag();
+
+    //add tags to main page
+    this.addTag(pingback);
+};
+
+function removeTag() {
+    if (document.getElementById('postCont') != null && document.getElementById('postCont') != undefined) {
+        document.getElementById('postCont').remove();
+    };
+};
+
+function addTag(tagsData) {
+    //create container tag
+    const postContainer = document.createElement('div');
+    postContainer.setAttribute('id', 'postCont');
+    allPostsTag.appendChild(postContainer);
+
+    //create all post tags with title and links
+    for (var elem in tagsData) {
+        let title = tagsData[elem].title.toString().trim();
+        let href = tagsData[elem].href.toString().trim();
+
+        const divTag = document.createElement('li');
+        divTag.setAttribute('href', href);
+        divTag.setAttribute('class', 'individualPosts');
+        divTag.textContent = title;
+        postContainer.appendChild(divTag);
+    };
+};
--- Server/app.js
+++ Server/app.js
@@ -27,6 +27,7 @@ app.get('/', (req, res) => {
     })
 });
 
+//New post added
 app.post('/newpost', (req, res) => {
     const db = monk(url);
     var title = beforeAfter(">", "</", req.body.content.toString())
@@ -66,6 +67,81 @@ app.post('/newpost', (req, res) => {
             res.end(JSON.stringify({
                 message: "error"
             }))
+        });
+});
+
+//login request handler
+app.post('/login', (req, res) => {
+    const db = monk(url);
+
+    var verifObject = {
+        title: title.trim(),
+    };
+
+    db.then(() => {
+            console.log('Connected correctly to db server');
+
+            const articleCollection = db.get('Articles');
+            console.log("collection opened");
+
+            articleCollection.insert(insertObject)
+                .then(() => console.log("data inseterd"))
+                .then(() => {
+                    db.close();
+                    addNewPage(title);
+                    res.status(200);
+                    res.end(JSON.stringify({
+                        message: "posted"
+                    }))
+                })
+                .catch((err) => {
+                    db.close();
+                    Promise.reject(err);
+                    res.status(400);
+                    res.end(JSON.stringify({
+                        message: "error"
+                    }))
+                })
+        })
+        .catch((err) => {
+            Promise.reject(err);
+            res.status(400);
+            res.end(JSON.stringify({
+                message: "error"
+            }))
+        });
+});
+
+//main page handler
+app.post('/main', (req, res) => {
+    const db = monk(url);
+
+    db.then(() => {
+            console.log('Connected correctly to db server');
+
+            const articleCollection = db.get('Articles');
+            console.log("collection opened");
+
+            articleCollection.find()
+                .then(() => console.log("fetched all"))
+                .then(() => {
+                    db.close();
+                    res.status(200);
+                    res.end(JSON.stringify({
+                        //put response
+                    }))
+                })
+                .catch((err) => {
+                    db.close();
+                    Promise.reject(err);
+                    res.status(400);
+                    res.render('error');
+                })
+        })
+        .catch((err) => {
+            Promise.reject(err);
+            res.status(400);
+            res.render('error');
         });
 });
 
